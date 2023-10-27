cead84d2e7245a2a2b0fb870cad590c8731bcfe1
lukeisun <lukeisun57@gmail.com>
2023-10-26 22:12:32 -0400

    done for tn

---
+++ CH1/1.3/1.43.scm
@@ -0,0 +1,11 @@
+#lang sicp
+(define (compose f g)
+  (lambda (x) (f (g x))))
+(define (repeated f n)
+  (define (iter i acc)
+    (if (= i n)
+        acc
+        (iter (+ i 1) (compose f acc))))
+  (iter 1 f))
+(define (square x) (* x x))
+((repeated square 2) 10)
