7901e2349673c3685bed52267d13172e09e4a714
lukeisun <lukeisun57@gmail.com>
2023-11-08 23:00:13 -0500

    2.3[3-6]

---
+++ CH2/2.2/2.35.scm
@@ -0,0 +1,15 @@
+#lang sicp
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(define (is-leaf? t)
+  (if (pair? t)
+      (+ (count-leaves (car t))
+         (count-leaves (cdr t)))
+      1))
+(define (count-leaves t)
+  (accumulate + 0 (map is-leaf? t)))
+(define x (cons (list 1 2) (list 3 4)))
+(count-leaves (list x x))
